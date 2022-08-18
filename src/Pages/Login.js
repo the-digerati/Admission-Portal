323b36edc6b60b6ce3bd02e749158484a1c26fe4
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-18 11:23:09 +0000

    Added the Registration Page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Login.css'
+import STS from '../sts-logo.png'
 
 const Login = () => {
   return (
     <div className='main'>
-
+        <img src={STS}></img>
     </div>
   )
 }
