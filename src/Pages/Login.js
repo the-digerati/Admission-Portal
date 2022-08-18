b23f4a29d6f3bbbfe2208ad08fcf97e55c507b5f
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-18 15:44:16 +0000

    Added some styles

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -10,16 +10,25 @@ const Login = () => {
       {/* User Status Selector */}
       <div className='user-status-selector'>
         <div className='student-status'>
-          <input type="radio" class="default:ring-2" id='student'/>
-          <span class="block text-sm font-medium text-slate-700" id='admin' style={{color: 'white'}}>Student</span>
+          <input type="radio" class="default:ring-2" id='student' />
+          <span class="block text-sm font-medium text-slate-700" id='admin' style={{ color: 'white' }}>Student</span>
         </div>
         <div className='admin-status'>
           <input type="radio" class="default:ring-1" />
-          <span class="block text-sm font-medium text-slate-700" style={{color: 'white'}}>Admin</span>
+          <span class="block text-sm font-medium text-slate-700" style={{ color: 'white' }}>Admin</span>
         </div>
       </div>
 
-
+      {/* Login Form */}
+      <form>
+        <label class="block">
+          <span class="block text-sm font-medium text-slate-700">Social Security Number</span>
+          <input class="border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
+          <p class="mt-2 opacity-10 contrast-more:opacity-100 text-slate-600 text-sm">
+            We need this to steal your identity.
+          </p>
+        </label>
+      </form>
     </div>
   )
 }
