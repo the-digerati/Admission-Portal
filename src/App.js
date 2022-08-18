7aa723d3937d8c95e58d96de3fb96792fa2df783
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-18 11:27:02 +0000

    Updating the Registration Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom'
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import Admission from './Pages/Admission'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element = {<Login/>}/>
         <Route path='/home' element = {<Home/>}/>
+        <Route path={'/register' ||'admission'} element = {<Admission/>}/>
       </Routes>
     </Router>
   );
