7392830bba054aa46a2b616e03301a5680186fb0
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-18 08:39:47 +0000

    Added the Login Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom'
 import Home from './Pages/Home';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element = {<Home/>}/>
+        <Route path='/' element = {<Login/>}/>
+        <Route path='/home' element = {<Home/>}/>
       </Routes>
     </Router>
   );
