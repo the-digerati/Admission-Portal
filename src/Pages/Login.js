498f6338e5d728f28e3a2e84528c2bba05bfbcae
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-18 16:04:11 +0000

    Updated the Login Page with the Butto

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -21,11 +21,15 @@ const Login = () => {
 
       {/* Login Form */}
       <label class="block">
-        <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-          Email
-        </span>
-        <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
+        <input type="text" name="USER-ID" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:ring-sky-500 block w-full rounded-md sm:text-sm" placeholder="USER ID" >
+
+        </input>
+
+        <input type="password" name="PIN" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:ring-sky-500 block w-full rounded-md sm:text-sm " placeholder="PIN"></input>
       </label>
+      <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
+        Save changes
+      </button>
     </div>
   )
 }
