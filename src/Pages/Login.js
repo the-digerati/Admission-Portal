22086b2dcd92112678b2fdd3b7cd08196ce916ae
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-18 15:15:20 +0000

    Updating the login Page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -5,8 +5,21 @@ import STS from '../sts-logo.png'
 const Login = () => {
   return (
     <div className='main'>
-        <img src={STS}></img>
-        <p></p>
+      <img src={STS}></img>
+
+      {/* User Status Selector */}
+      <div className='user-status-selector'>
+        <div className='student-status'>
+          <input type="radio" class="default:ring-2" id='student'/>
+          <span class="block text-sm font-medium text-slate-700" id='admin'>Student</span>
+        </div>
+        <div className='admin-status'>
+          <input type="radio" class="default:ring-2" />
+          <span class="block text-sm font-medium text-slate-700">Admin</span>
+        </div>
+      </div>
+
+
     </div>
   )
 }
