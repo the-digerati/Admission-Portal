8fde0308ff17281b180ed5ccd35a63a0737cd880
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-21 21:06:10 +0000

    Added styling

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Menu.css'
 
+
 const Menu = () => {
   return (
     <div className='container hero-section'>
       {/* Aside Menu */}
       <div className='aside-menu'>
+        
 
       </div>
         
