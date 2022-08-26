9fb5a45b921bb222428b76631c62e24dde8ecc82
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-26 18:58:48 +0000

    Added icons and sourced them

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Menu.css'
 import chevron from '../Icons/chevron.svg'
+import broadcast from '../Icons/broadcast.svg'
+import cash from '../Icons/cash.svg'
+import logout from '../Icons/logout.svg'
+import profile from '../Icons/profile.svg'
+import settings1 from '../Icons/settings1.svg'
+import Vector from '../Icons/Vector.svg'
 
 
 const Menu = () => {
@@ -9,7 +15,7 @@ const Menu = () => {
       {/* Aside Menu */}
       <div className='aside-menu'>
         <ul className='menu-nav'>
-          <li><img src={chevron} /></li>
+          <li><img src={chevron}/>Less</li>
           <li><img src={chevron} />Ghana</li>
           <li><img src={chevron} />Ghana</li>
           <li><img src={chevron} />Ghana</li>
