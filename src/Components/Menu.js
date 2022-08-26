56a57b341df2490408050764dca09d4857163234
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-26 19:10:08 +0000

    Added new components

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -21,10 +21,10 @@ const Menu = () => {
         <ul className='menu-nav'>
           <li><img src={chevron}/>Less</li>
           <li><img src={cash} />Fees</li>
-          <li><img src={broadcast} />Ghana</li>
-          <li><img src={settings1} />Ghana</li>
-          <li><img src={settings} />Ghana</li>
-          <li><img src={Vector} />Ghana</li>
+          <li><img src={broadcast} />Profile</li>
+          <li><img src={settings1} />Settings</li>
+          <li><img src={settings} />Status</li>
+          <li><img src={Vector} />Quiz</li>
           <li><img src={Pen} />Ghana</li>
           <li><img src={radio} />Ghana</li>
           <li><img src={mail} />Ghana</li>
