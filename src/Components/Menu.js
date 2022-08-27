156495a594a6805620a8914ef11089789d12c0fb
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-27 19:54:34 +0000

    Added new components

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -13,22 +13,26 @@ import Vector from '../Icons/Vector.svg'
 import radio from '../Icons/codicon_broadcast.svg'
 
 
-const Menu = () => {
+const Menu = () => 
+  const minimize = () => {
+    let min = document.querySelector('#minimize');
+    min.addEventListener('click', minimize);
+  }
   return (
     <div className='container hero-section'>
       {/* Aside Menu */}
       <div className='aside-menu'>
         <ul className='menu-nav'>
-          <li><img src={chevron}/>Less</li>
-          <li><img src={cash} />Fees</li>
+          <li><img src={chevron} id = 'minimize'/>Less</li>
+          <li><img src={Pen} />Enrol</li>
+          <li><img src={mail} />Inbox</li>
           <li><img src={broadcast} />Profile</li>
-          <li><img src={settings1} />Settings</li>
+          <li><img src={cash} />Fees</li>
           <li><img src={settings} />Status</li>
           <li><img src={Vector} />Quiz</li>
-          <li><img src={Pen} />Ghana</li>
-          <li><img src={radio} />Ghana</li>
-          <li><img src={mail} />Ghana</li>
-          <li><img src={logout} />Ghana</li>
+          <li><img src={settings1} />Settings</li>
+          <li><img src={radio} />Broadcast</li>
+          <li><img src={logout} />Logout</li>
 
 
         </ul>
