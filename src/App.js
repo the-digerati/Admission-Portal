67d4b2e8f3d5e12a20288a99d04be82e7c75498c
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-29 14:26:17 +0000

    Added some file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom'
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Admission from './Pages/Admission'
+import Quiz from './Pages/Quiz'
+import Fees from './Pages/Fees'
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Routing Path to different Pages */}
         <Route path='/' element = {<Login/>}/>
         <Route path='/home' element = {<Home/>}/>
         <Route path='/register' element = {<Admission/>}/>
+        <Route path='/quiz' element = {<Quiz/>}/>
+        <Route path='/pay-fees' element = {<Fees/>}/>
       </Routes>
     </Router>
   );
