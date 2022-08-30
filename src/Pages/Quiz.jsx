15f4165fa820f4c028a1adb3405eb6f820628ba7
John Teye Doku <johnteye2002@gmail.com>
2022-08-30 16:03:10 +0000

    Updated-Quiz-Page

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
+import { FcGlobe, FcCalculator, FcBiotech, FcReading } from "react-icons/fc";
+
 import "./NewStyles.css";
 
 const Quiz = () => {
@@ -12,14 +14,33 @@ const Quiz = () => {
         <h2>Available Quizzes</h2>
         <div className="quiz-section">
           <div className="quiz-card">
-            <p></p>
+            <span className="circle">
+              <FcReading className="icon"></FcReading>
+            </span>
+            <p>English Language</p>
           </div>
-          <div className="quiz-card"></div>
-          <div className="quiz-card"></div>
-          <div className="quiz-card"></div>
-         
+          <div className="quiz-card">
+            <span className="circle">
+              <FcCalculator className="icon" />
+            </span>
+            <p>Mathematics</p>
+          </div>
+          <div className="quiz-card">
+            <span className="circle">
+              <FcBiotech className="icon" />
+            </span>
+            <p>Integrated Science</p>
+          </div>
+          <div className="quiz-card">
+            <span className="circle">
+              <FcGlobe className="icon" />
+            </span>
+            <p>Social Studies</p>
+          </div>
         </div>
       </div>
+
+      <div className="questionaire"></div>
     </div>
   );
 };
