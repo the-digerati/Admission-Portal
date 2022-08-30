e8d234b13ba64d125c8fe46cab708741841b5653
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-30 23:09:41 +0000

    Creating the main quiz card

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -2,46 +2,37 @@ import React from "react";
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
 import "./NewStyles.css";
+// import
 
 const Quiz = () => {
   return (
-    <div className="main-page">
-      <Header />
-      <Menu />
-      <div className="quiz-pane">
-        <h2>Available Quizzes</h2>
-        <div className="quiz-section">
-          <div className="quiz-card">
-            <span className="circle"></span>
-            <p>English</p>
+    <div className="quiz-page">
+      <div className="main-page">
+        <Header />
+        <Menu />
+        <div className="quiz-pane">
+          <h2>Available Quizzes</h2>
+          <div className="quiz-section">
+            <div className="quiz-card">
+              <span className="circle"></span>
+              <p>English Language</p>
+            </div>
+
+            <div className="quiz-card">
+              <span className="circle"></span>
+              <p>Mathematics</p>
+            </div>
+
+            <div className="quiz-card">
+              <span className="circle"></span>
+              <p>Integrated Science</p>
+            </div>
+
+            <div className="quiz-card">
+              <span className="circle"></span>
+              <p>Social Studies</p>
+            </div>
           </div>
-    
-          <div className="quiz-card">
-            <span className="circle"></span>
-            <p>Mathematics</p>
-          </div>
-    
-          <div className="quiz-card">
-            <span className="circle"></span>
-            <p>Science</p>
-          </div>
-    
-          <div className="quiz-card">
-            <span className="circle"></span>
-            <p>Social Studies</p>
-          </div>
-    
-          <div className="quiz-card">
-            <span className="circle"></span>
-            <p>Mathematics</p>
-          </div>
-    
-          <div className="quiz-card">
-            <span className="circle"></span>
-            <p>French</p>
-          </div>
-    
-         
         </div>
       </div>
     </div>
