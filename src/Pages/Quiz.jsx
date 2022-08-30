b30c0e847b339ff879c80fdf20fe69e4bad0f888
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-30 09:47:08 +0000

    Added some components

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Header from '../Components/Header'
-import Menu from '../Components/Menu'
-import './NewStyles.css'
+import React from "react";
+import Header from "../Components/Header";
+import Menu from "../Components/Menu";
+import "./NewStyles.css";
 
 const Quiz = () => {
   return (
-    <div className='main-page'>
-      <Header/>
-      <Menu/>
-      <div  className='quiz-pane'>
-        <h2></h2>
+    <div className="main-page">
+      <Header />
+      <Menu />
+      <div className="quiz-pane">
+        <h2>Available Quizzes</h2>
+        <div className="quiz-section">
+          <div className="quiz-card"></div>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
