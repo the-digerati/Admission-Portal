c9f8c97686d57750fe6eefcef05896fe32336d2a
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-31 17:12:13 +0000

    Added some components for the qualification page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,21 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
 import "./NewStyles.css";
 import "./Home.css";
 
 const Profile = () => {
-
-
   return (
     <div className="main-page">
       <Header />
       <Menu />
 
-        <div className="profile">
-            
+      <div className="profile-main">
+        <div className="user-profile"></div>
+        <div className="user-qualification">
+            <span id="score" className="quiz-marks-field">-</span>
         </div>
-
+      </div>
     </div>
   );
 };
