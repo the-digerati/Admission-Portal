e6d2f17f0a6dd771523aab64ed77751a3b96ecd8
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-31 14:45:12 +0000

    Added Teleco Images

--- src/Pages/Fees.jsx
+++ src/Pages/Fees.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import Header from '../Components/Header';
-import Menu from '../Components/Menu';
+import React from "react";
+import Header from "../Components/Header";
+import Menu from "../Components/Menu";
 import "./NewStyles.css";
+import "./Home.css";
+
+// Images
+import Airtel from "../airtelTigo.png"
 
 const Fees = () => {
   return (
-    <div className= "main-page">
-    <Header/>
-    <Menu/>
-    </div>
-  )
-}
+    <div className="main-page">
+      <Header />
+      <Menu />
+      {/* Payment Card */}
+      <div className="fees-page">
+        <h1>Pay  Your Fees Now</h1>
+        <p>Kindly choose the payment mode below for further assistance</p>
 
-export default Fees
+        <div className="teleco-main">
+          <div className="teleco">
+            <img src={Airtel}/>
+          </div>
+          <div className="teleco"></div>
+          <div className="teleco"></div>
+          <div className="teleco"></div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Fees;
