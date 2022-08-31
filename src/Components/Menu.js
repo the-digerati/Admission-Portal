16964907449a62f925a1a6d9af8fd9ce4096ff4f
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-31 16:16:14 +0000

    Styled the profile page

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -15,6 +15,7 @@ import { Link } from 'react-router-dom'
 
 
 const Menu = () => {
+  
   return (
     <div className='container hero-section'>
       {/* Aside Menu */}
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
 import "./NewStyles.css";
 import "./Home.css";
 
 const Profile = () => {
+
+
   return (
     <div className="main-page">
       <Header />
       <Menu />
+
+        <div className="profile">
+            
+        </div>
+
     </div>
   );
 };
