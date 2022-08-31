f70816c38b31a28b8b6cad41bd8200fabdd7affc
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-31 17:28:21 +0000

    Profile Page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -13,7 +13,15 @@ const Profile = () => {
       <div className="profile-main">
         <div className="user-profile"></div>
         <div className="user-qualification">
-            <span id="score" className="quiz-marks">-</span>
+            <span id="score" className="quiz-marks">--</span>
+            <span className="quiz-statement">is your score for the entrance quiz.</span>
+            <p>Check your status below.</p>
+
+            <span id="admission-status">
+                <span className= "status-image"></span>
+            </span>
+            <span id="admission-status"></span>
+            <span id="admission-status"></span>
         </div>
       </div>
     </div>
