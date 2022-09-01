a09104487d7c6e4cc080100fd431cea5de87fbfd
John Teye Doku <johnteye2002@gmail.com>
2022-09-01 22:57:58 +0000

    firebaseupdate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom'
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Admission from './Pages/Admission'
-import Quiz from './Pages/Quiz'
-import Fees from './Pages/Fees'
-import Profile from './Pages/Profile';
-import AdminDashboard from './Pages/AdminDashboard';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Admission from "./Pages/Admission";
+import Quiz from "./Pages/Quiz";
+import Fees from "./Pages/Fees";
+import Profile from "./Pages/Profile";
+import AdminDashboard from "./Pages/AdminDashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Routing Path to different Pages */}
-        <Route path='/' element = {<Login/>}/>
-        <Route path='/home' element = {<Home/>}/>
-        <Route path='/apply' element = {<Admission/>}/>
-        <Route path='/quiz' element = {<Quiz/>}/>
-        <Route path='/pay-fees' element = {<Fees/>}/>
-        <Route path='/profile' element = {<Profile/>}/>
-        <Route path='/admin' element = {<AdminDashboard/>}/>
-        <Route path='/settings' element = {<Profile/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/apply" element={<Admission />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/pay-fees" element={<Fees />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/settings" element={<Profile />} />
       </Routes>
     </Router>
   );
--- src/Pages/Admission.jsx
+++ src/Pages/Admission.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
-import { db } from "./firebase";
+import { db } from "./storage";
 import "./NewStyles.css";
 
 const Admission = () => {
