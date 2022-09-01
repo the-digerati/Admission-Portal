54d9be73cbadc2fecd919d745b2c8b1ab35ff79e
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-09-01 19:23:15 +0000

    Styling

--- src/Pages/firebase.js
+++ src/Pages/firebase.js
@@ -1,16 +1,16 @@
-import firebase from "firebase";
+// import firebase from "firebase";
 
-var firebaseApp = (firebase.initializeApp = {
-  apiKey: "AIzaSyCTxzOWsfYy9ChKMFyUzgzUeuSbWgSiuFs",
-  authDomain: "admission-portal-21ea9.firebaseapp.com",
-  databaseURL: "https://admission-portal-21ea9-default-rtdb.firebaseio.com",
-  projectId: "admission-portal-21ea9",
-  storageBucket: "admission-portal-21ea9.appspot.com",
-  messagingSenderId: "444647914260",
-  appId: "1:444647914260:web:e64e312c33f8dc800d2001",
-  measurementId: "G-8WW4F0R7HZ",
-});
+// var firebaseApp = (firebase.initializeApp = {
+//   apiKey: "AIzaSyCTxzOWsfYy9ChKMFyUzgzUeuSbWgSiuFs",
+//   authDomain: "admission-portal-21ea9.firebaseapp.com",
+//   databaseURL: "https://admission-portal-21ea9-default-rtdb.firebaseio.com",
+//   projectId: "admission-portal-21ea9",
+//   storageBucket: "admission-portal-21ea9.appspot.com",
+//   messagingSenderId: "444647914260",
+//   appId: "1:444647914260:web:e64e312c33f8dc800d2001",
+//   measurementId: "G-8WW4F0R7HZ",
+// });
 
-var db = firebaseApp.firestore();
+// var db = firebaseApp.firestore();
 
-export { db };
+// export { db };
