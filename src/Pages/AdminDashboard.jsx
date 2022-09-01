524fc804b1f08fa167d412d798bfa789e4251edf
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-09-01 15:12:52 +0000

    Added the chart into the chart div

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from "react";
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
 import "./NewStyles.css";
 
 const AdminDashboard = () => {
-    return (
-        <div className='main-page'>
-            <Header userName = "Administrator"/>
-            <Menu/>
-            <div className="dashboard">
-                
-            </div>
-            
+  return (
+    <div className="main-page">
+      <Header userName="Administrator" />
+      <Menu />
+      <div className="dashboard">
+        <div className="top-chart">
+            <div className="chart"></div>
+            <div className="chart"></div>
+            <div className="chart"></div>
+            <div className="chart"></div>
 
         </div>
-    )
-}
+        
+        <div className="active-user-details" id="appliedUsers">
+            <h2 className = 'dashboard-title'> Enrolled Students</h2>
+            <div id="appliedUser"></div>
+            <div id="appliedUser"></div>
+            <div id="appliedUser"></div>
+            <div id="appliedUser"></div>
+            <div id="appliedUser"></div>
+            <div id="appliedUser"></div>
+            <div id="appliedUser"></div>
 
-export default AdminDashboard
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
