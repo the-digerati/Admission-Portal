c007c75a98cc050f8e7379366af8e0522d755d68
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-09-22 08:49:28 +0000

    Added some new comments

--- src/Pages/Fees.jsx
+++ src/Pages/Fees.jsx
@@ -4,7 +4,7 @@ import Menu from "../Components/Menu";
 import "./NewStyles.css";
 import "./Home.css";
 
-// Images for Payment Card Page
+// Images for Payment Card Pages
 
 
 import Airtel from "../airtelTigo.png"
